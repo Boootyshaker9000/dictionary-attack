f8808d148b5c8904ca6eea1ea91d9d83a25029a6
Boootyshaker9000 <relich@post.cz>
2025-11-29 13:48:49 +0100

    Added documentation for ResultSaver.cs.

--- Services/ResultSaver.cs
+++ Services/ResultSaver.cs
@@ -1,7 +1,15 @@
 namespace PasswordCracker.Services;
 
+/// <summary>
+/// Static class for saving output into a text file
+/// </summary>
 public static class ResultSaver
 {
+    /// <summary>
+    /// Saves matches into a text file
+    /// </summary>
+    /// <param name="filePath">File path of the output file</param>
+    /// <param name="matches">List of pairs of passwords and matched hashes</param>
     public static void SaveMatches(string filePath, List<(string password, string hash)> matches)
     {
         using var matchFile = File.CreateText(filePath);
